ac69440fb633f6dffd796d8e4262152750ac94b3
Jack O'Hara <jack.ohara@infinityworks.com>
2023-07-18 23:07:34 +0100

    feat: added the ui to the cdk stack

--- planets-api/infra/src/Infra/PlanetsStack.cs
+++ planets-api/infra/src/Infra/PlanetsStack.cs
@@ -3,6 +3,10 @@ using Amazon.CDK;
 using Amazon.CDK.AWS.DynamoDB;
 using Amazon.CDK.AWS.Lambda;
 using Amazon.CDK.AWS.Logs;
+using Amazon.CDK.AWS.S3;
+using Amazon.CDK.AWS.S3.Deployment;
+using Amazon.CDK.AWS.CloudFront;
+using Amazon.CDK.AWS.CloudFront.Origins;
 using Constructs;
 
 namespace Infra
@@ -19,7 +23,7 @@ namespace Infra
                 BillingMode = BillingMode.PAY_PER_REQUEST
             });
 
-            var lambdaFunctionOne = new Function(this, "planets-api", new FunctionProps
+            var planetsApiLambda = new Amazon.CDK.AWS.Lambda.Function(this, "planets-api", new Amazon.CDK.AWS.Lambda.FunctionProps
             {
                 Runtime = Runtime.DOTNET_6,
                 MemorySize = 1024,
@@ -48,6 +52,8 @@ namespace Infra
                 }),
             });
 
+            dynamoTable.GrantReadData(planetsApiLambda);
+
             // I ended up having to add a HTTP api manually through the console
             // because of a CORS error I didn't have time to properly understand
 
@@ -63,9 +69,30 @@ namespace Infra
             //    },
             //});
 
-            dynamoTable.GrantReadData(lambdaFunctionOne);
-
             //new CfnOutput(this, "apigwarn", new CfnOutputProps { Value = restAPI.ArnForExecuteApi() });
+
+            var uiS3Bucket = new Bucket(this, "planets-ui-bucket", new BucketProps
+            {
+                AccessControl = BucketAccessControl.PRIVATE
+            });
+
+            new BucketDeployment(this, "planets-ui-bucket-deployment", new BucketDeploymentProps
+            {
+                DestinationBucket = uiS3Bucket,
+                Sources = new[] { Source.Asset("../../planets-ui/dist") }
+            });
+
+            var originAccessIdentity = new OriginAccessIdentity(this, "origin-access-identity");
+            uiS3Bucket.GrantRead(originAccessIdentity);
+
+            new Distribution(this, "planets-cloudfront-distribution", new DistributionProps
+            {
+                DefaultRootObject = "index.html",
+                DefaultBehavior = new BehaviorOptions
+                {
+                    Origin = new S3Origin(uiS3Bucket, new S3OriginProps { OriginAccessIdentity = originAccessIdentity })
+                } 
+            });
         }
     }
 }
